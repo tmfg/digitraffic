da29f9db1d3a5ee172718e81a4e773baf09c491d
TomNuorivaara-Solita <tom.nuorivaara@solita.fi>
2017-08-28 16:58:23 +0300

    Aukeava valikko lisätty

--- _site/js/script.js
+++ _site/js/script.js
@@ -1,8 +1,22 @@
 /***Write javascript under this line***/
 
-/* Add .header--scrolled when scrolling page */
+/* Check if DOM is ready */
 "use strict";
 
+var domResolve = undefined;
+var domReady = new Promise(function (resolve) {
+  domResolve = resolve;
+});
+
+document.addEventListener('DOMContentLoaded', domResolve);
+domReady.then(init);
+
+function init() {
+  console.log("Dom ready");
+  addToggleMenu();
+}
+
+/* Add .header--scrolled when scrolling page */
 window.onscroll = function () {
   var headerClasses = document.body.querySelector(".header").classList;
 
@@ -16,3 +30,20 @@ window.onscroll = function () {
     }
   }
 };
+
+/* Open and close menu */
+function addToggleMenu() {
+  var menuElement = document.body.querySelector(".header-menu__item--menu");
+
+  if (menuElement) {
+    menuElement.addEventListener("click", function () {
+      var headerClasses = document.body.querySelector(".header").classList;
+
+      if (headerClasses.contains("header--menu-opened")) {
+        headerClasses.remove("header--menu-opened");
+      } else {
+        headerClasses.add("header--menu-opened");
+      }
+    });
+  }
+}
--- js/script.js
+++ js/script.js
@@ -2,6 +2,21 @@
 ---
 /***Write javascript under this line***/
 
+/* Check if DOM is ready */
+let domResolve;
+let domReady = new Promise((resolve) => {
+  domResolve = resolve;
+});
+
+document.addEventListener('DOMContentLoaded', domResolve);
+domReady.then(init);
+
+function init() {
+  console.log("Dom ready");
+  addToggleMenu();
+}
+
+
 /* Add .header--scrolled when scrolling page */
 window.onscroll = () => {
   const headerClasses = document.body.querySelector(".header").classList;
@@ -14,5 +29,22 @@ window.onscroll = () => {
     if (!headerClasses.contains("header--scrolled")) {
       headerClasses.add("header--scrolled");
     }
+  }
+}
+
+/* Open and close menu */
+function addToggleMenu() {
+  let menuElement = document.body.querySelector(".header-menu__item--menu");
+  
+  if (menuElement) {
+    menuElement.addEventListener("click", () => {
+    const headerClasses = document.body.querySelector(".header").classList;
+    
+    if (headerClasses.contains("header--menu-opened")) {
+        headerClasses.remove("header--menu-opened");
+      } else {
+        headerClasses.add("header--menu-opened");
+      }
+    });
   }
 }
