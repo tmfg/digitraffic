a71b1ebb052738134f1e94f9de28848b674ef194
TomNuorivaara-Solita <tom.nuorivaara@solita.fi>
2017-10-10 10:56:18 +0300

    Swagger-kuvauksen tyylejä korjattu. Luotu bash-skripti, jolla päivitetään swagger-dokumentaatio

---
+++ update-swagger.sh
@@ -0,0 +1,3 @@
+curl https://tie.digitraffic.fi/api/v1/metadata/documentation/v2/api-docs\?group\=metadata-api -o _data/road.yml
+curl https://rata.digitraffic.fi/swagger/swagger.json -o _data/rail.yml
+curl https://meri.digitraffic.fi/api/v1/metadata/documentation/v2/api-docs?group=metadata-api -o _data/marine.yml
