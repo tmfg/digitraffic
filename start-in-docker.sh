062d99b7d565607feaad875abfe4a93725c9393c
Petri Sirkkala <sirpete@iki.fi>
2018-01-17 10:40:49 +0200

    Siivottu käynnistysskriptiä

--- start-in-docker.sh
+++ start-in-docker.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker rm digitraffic; docker build . -t my/digitraffic; docker run --rm --name=digitraffic -p 3000:3000 -p 3001:3001 -v ${PWD}:/app my/digitraffic:latest
+docker build . -t my/digitraffic
+docker run --rm --name=digitraffic -p 3000:3000 -p 3001:3001 -v ${PWD}:/app my/digitraffic:latest
