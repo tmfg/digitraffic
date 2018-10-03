8fd6df84c8552b0473dde3b421565a12b529936c
Petri Sirkkala <sirpete@iki.fi>
2018-10-03 12:10:22 +0300

    DPO-548 Palautettu TRAP komento, jotta kehitysympäristön voi sammuttaa - TRAP sulkee kehitysympäristössä start-in-docker.sh:lla käynnistetyn   sovelluksen. Muuten sovellusta ei voi sulkea ilman docker loitsuja.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Signal handler to clean up and exit properly when terminating
+trap "echo Exiting...; kill -TERM -1; exit" SIGINT SIGTERM
+
 bundle install
 npm install
 npm run dev &
