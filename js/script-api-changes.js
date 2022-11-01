eb7116a23dfe8718c428e911a45f84ec1635b94b
Ilari Junnonen <ilari.junnonen@solita.fi>
2022-11-01 13:09:40 +0200

    DPO-1827 Add description and color code deprecation dates

--- js/script-api-changes.js
+++ js/script-api-changes.js
@@ -10,6 +10,7 @@ const translations = {
     en: {
         api: "API",
         deprecationsHeader: "Deprecated APIs",
+        deprecationsText: "An API is considered deprecated once a new version of it is released. Deprecated APIs will be available for a period of 6 months after the release of a new version. However, deprecated APIs are not recommended for use, and users should move to a supported version instead.",
         marine: "Marine",
         rail: "Rail",
         road: "Road",
@@ -19,18 +20,19 @@ const translations = {
     },
     fi: {
         api: "Rajapinta",
-        deprecationsHeader: "Deprekoidut rajapinnat",
+        deprecationsHeader: "Vanhentuneet rajapinnat",
+        deprecationsText: "Rajapinta katsotaan vanhentuneeksi, kun siitä julkaistaan uusi versio. Vanhentunut rajapinta on saatavilla 6kk ajan uuden version julkaisusta, mutta sitä ei suositella käytettäväksi, vaan käyttäjien tulisi siirtyä tuettuun versioon.",
         marine: "Meri",
         rail: "Rata",
         road: "Tie",
-        sunset: "Sunset-pvm",
+        sunset: "Poistuu",
         supportedHeader: "Tuetut rajapinnat",
         trafficType: "Liikennemuoto"
     }
 };
 
 const removalTextMatcher = /(?:W|w)ill be removed/
-const sunsetDateMatcher = RegExp(`(${removalTextMatcher.source})\.+(\\d{4}-\\d{2}-\\d{2})`);
+const sunsetDateMatcher = RegExp(`(${removalTextMatcher.source})\.+(?<sunsetDate>\\d{4}-\\d{2}-\\d{2})`);
 
 function initDeprecationsTable(trafficType, tableTitle, language) {
     $("#" + trafficType + "-DEPRECATIONS").append([
@@ -51,7 +53,7 @@ function initDeprecationsTable(trafficType, tableTitle, language) {
     ]);
 }
 
-function initSupportedTable(trafficType, tableTitle, language) {
+function initSupportedTable(trafficType, tableTitle) {
     $("#" + trafficType + "-SUPPORTED").append([
         $("<colgroup>").append([
             $("<col>", {"class": "api-changes-col"})
@@ -83,12 +85,14 @@ function loadApiDescription(trafficType) {
 function populateDeprecations(apiDescription, trafficType) {
     const deprecatedPaths = Object.keys(apiDescription.paths)
         .filter(path => apiDescription.paths[path].get.deprecated === true || removalTextMatcher.test(apiDescription.paths[path].get.summary))
-        .map(deprecatedPath => {
-            const extractedDate = apiDescription.paths[deprecatedPath].get.summary.match(sunsetDateMatcher);
-            const sunsetDate = extractedDate !== null ? extractedDate[2] : "TBD";
+        .map(path => {
+            const match = apiDescription.paths[path].get.summary.match(sunsetDateMatcher);
+            const sunset = match !== null ? match.groups.sunsetDate : "TBD";
+            const dateClass = sunset !== "TBD" ? getSunsetDateClass(sunset) : "";
             return {
-                path: deprecatedPath,
-                sunset: sunsetDate
+                path,
+                sunset,
+                dateClass
             }
         });
 
@@ -104,6 +108,18 @@ function populateDeprecations(apiDescription, trafficType) {
     }
 }
 
+function getSunsetDateClass(isoLocalDateString) {
+    const weekInMilliSeconds = 1000 * 60 * 60 * 24 * 7;
+    const comparisonDate = new Date(isoLocalDateString);
+    const differenceInWeeks = (comparisonDate - new Date()) / weekInMilliSeconds;
+
+    if (differenceInWeeks <= 4) {
+        return "deprecations-sunset-alert";
+    } else if (differenceInWeeks <= 12) {
+        return "deprecations-sunset-warning"
+    } else return "";
+}
+
 function populateSupported(apiDescription, trafficType) {
     Object.keys(apiDescription.paths)
         .filter(path => apiDescription.paths[path].get.deprecated !== true && !removalTextMatcher.test(apiDescription.paths[path].get.summary))
@@ -115,7 +131,7 @@ function addToDeprecationsTable(api, trafficType) {
     $('#' + trafficType + '-DEPRECATIONS > tbody:last-child').append(
         $('<tr/>', {"class": "row.nowrap"}).append([
             $('<td/>', {"class": "deprecations-col1"}).text(api.path),
-            $('<td/>', {"class": "deprecations-col2"}).text(api.sunset)
+            $('<td/>', {"class": `deprecations-col2 ${api.dateClass}`}).text(api.sunset)
         ])
     );
 }
@@ -128,12 +144,15 @@ function addToSupportedTable(path, trafficType) {
     );
 }
 
-function addHeaders(language) {
+function addHeadersAndText(language) {
     $('#DEPRECATIONS-HEADER').append(
         $('<h3 />').text(translations[language].deprecationsHeader)
     );
     $('#SUPPORTED-HEADER').append(
         $('<h3 />').text(translations[language].supportedHeader)
+    );
+    $('#DEPRECATIONS-TEXT').append(
+        $('<p />').text(translations[language].deprecationsText)
     );
 }
 
@@ -143,7 +162,7 @@ function removeEmptyDeprecationsTable(trafficType) {
 }
 
 async function loadApiChanges(language) {
-    addHeaders(language);
+    addHeadersAndText(language);
 
     initDeprecationsTable("MARINE", translations[language].marine.toUpperCase(), language);
     initDeprecationsTable("RAIL", translations[language].rail.toUpperCase(), language);
--- pages/rajapintojen-muutokset.md
+++ pages/rajapintojen-muutokset.md
@@ -5,5 +5,5 @@ permalink: /rajapintojen-tila/muutokset/
 title: Rajapintojen muutokset
 lang: fi
 ref: api-changes
-intro: Deprekoidut ja tuetut rajapinnat
+intro: Vanhentuneiden ja tuettujen rajapintojen listaus
 ---
