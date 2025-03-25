0a0d10b4dcb023a495ce3bc51ddec829c0c71df2
Joonas-M-S <joonas.mikkonen@solita.fi>
2025-03-25 17:52:32 +0200

    Fix search functionality

--- src/assets.js
+++ src/assets.js
@@ -14,7 +14,12 @@ import filter_developments from "./js/filter-developments";
 import filter_posts from "./js/filter-posts";
 import script_api_changes from "./js/script-api-changes.js";
 import script_twc from "./js/script-twc";
-import { mqtt_reconnect, mqtt_disconnect, mqtt_updateTopicTemplate } from "./js/script-mqtt";
+import {
+  mqtt_disconnect,
+  mqtt_reconnect,
+  mqtt_updateTopicTemplate,
+} from "./js/script-mqtt";
+import * as digitrafficSearch from "./js/search";
 
 /** Compiled JavaScript files **/
 import script_api_status, { getServiceStatus } from "../dist/script-api-status";
@@ -27,6 +32,7 @@ window.tippy = tippy;
 window.mqtt_reconnect = mqtt_reconnect;
 window.mqtt_disconnect = mqtt_disconnect;
 window.mqtt_updateTopicTemplate = mqtt_updateTopicTemplate;
+window.digitrafficSearch = digitrafficSearch;
 
 // Init highlight.js https://highlightjs.org/
 window.hljs = hljs;
