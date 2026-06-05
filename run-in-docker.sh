88deb8bfd59f6566a5acb1ceb8ea46689125063a
Teijo Rönkä <teijo.ronka@solita.fi>
2026-06-05 14:26:58 +0300

    DPO-4658 use v2 version of traffic-message api

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -2,4 +2,4 @@
 rm -rf _site
 pnpm install
 pnpm build-and-install
-docker run --rm --name=digitraffic-pages -e "SKIP_UPDATE=${SKIP_UPDATE}" -p 3000:3000 -p 3001:3001 -v ${PWD}:/app digitraffic-pages:latest
+docker run --rm --name=digitraffic-pages -e "CI=true SKIP_UPDATE=${SKIP_UPDATE}" -p 3000:3000 -p 3001:3001 -v ${PWD}:/app digitraffic-pages:latest
