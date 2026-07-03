5241ccd697ca8b69d2ccb953ebaff07c0551738f
Ilari Junnonen <ilari.junnonen@solita.fi>
2026-07-03 15:03:51 +0300

    fix local env run script

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-rm -rf _site node_modules
+rm -rf _site
 pnpm install
+pnpm run setup
 pnpm build-and-install
 # add this to docker command if needed! -e "CI=true"
-docker run --rm --name=digitraffic-pages -e "CI=true" -e "SKIP_UPDATE=${SKIP_UPDATE}" -p 3000:3000 -p 3001:3001 -v ${PWD}:/app digitraffic-pages:latest 
+docker run --rm --name=digitraffic-pages -e "CI=true" -e "SKIP_UPDATE=${SKIP_UPDATE}" -p 3000:3000 -p 3001:3001 -v ${PWD}:/app -v /app/node_modules digitraffic-pages:latest 
